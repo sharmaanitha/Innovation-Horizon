c0786691b4cf73e12a1c41cdadb0508143308306
sharmaanitha <71346537+sharmaanitha@users.noreply.github.com>
2020-11-16 09:45:22 -0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,1 @@
+alert("Hello and Welcome to Innovation Horizon!")
---
+++ popup.js
@@ -0,0 +1,6 @@
+var proceed = confirm("Are you sure you want to leave this page?");
+if (proceed) {
+  //proceed
+} else {
+  //don't proceed
+}
